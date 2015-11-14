9c2aba7a34f87aabcb9aae44ac7d37428c8b7b78
Nicolas Adami-Sampson <nicolas.adamisampson@gmail.com>
2015-11-14 17:05:54 -0500

    Added comments. Late I know... Busy day.

--- src/com/team1ofus/hermes/HermesUI.java
+++ src/com/team1ofus/hermes/HermesUI.java
@@ -48,19 +48,24 @@ public class HermesUI extends JPanel{
 	*/
 	
 	private void initialize() {
+		
 		frameHermes = new JFrame();
 		frameHermes.setTitle("Hermes");
 		frameHermes.setBounds(0, 0, screenSize.width - 200, screenSize.height - 200);
+		//Set screen size to be the size of the display - 200
+		//kind of arbitrary
 		frameHermes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameHermes.getContentPane().setLayout(null);
 		frameHermes.setMinimumSize(new Dimension(800,600));
 		frameHermes.setLocation(screenSize.width/2-frameHermes.getSize().width/2, screenSize.height/2-frameHermes.getSize().height/2);
+		//Sets the frame to start in the center of the screen
 		
 		JPanel DestinationPanel = new JPanel();
 		DestinationPanel.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
 		DestinationPanel.setBackground(Color.LIGHT_GRAY);
 		DestinationPanel.setBounds(6, 6, screenSize.width - (screenSize.width - 134), screenSize.height - (screenSize.height - 100));
 		frameHermes.getContentPane().add(DestinationPanel);
+		//Destination panel placed in the upper right corner of the frameHermes
 		
 		StartField = new JTextField();
 		StartField.setText("Startpoint");
@@ -71,16 +76,25 @@ public class HermesUI extends JPanel{
 		DestinationField.setText("Destination");
 		DestinationPanel.add(DestinationField);
 		DestinationField.setColumns(10);
+		//Builds the two input fields and displays them in the DestinationPanel
+		
 		
 		JButton addDestination = new JButton("Add Destination");
 		addDestination.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
 		addDestination.setBorder(BorderFactory.createEmptyBorder());   
 		DestinationPanel.add(addDestination);
+		//Not complete yet, but will end up adding another field for more destinations
 		
 		JPanel MapPanel = new JPanel();
 		MapPanel.setBounds(0, 0, screenSize.width, screenSize.height);
 		//JLabel mapPlacer = new JLabel("",map2,JLabel.CENTER);
+		
+		//To inport an image you need to do a janky thing.
+		//Basically you place an image inside a JLabel, override the paintComponent method, and place it in the panel
+		
 		//MapPanel.add(mapPlacer);
+		//MapPanel is where the map is displayed
+		
 		
 		JPanel MousePanel = new JPanel();
 		JLabel mouseOut = new JLabel("#mouse#");
@@ -103,6 +117,7 @@ public class HermesUI extends JPanel{
 			}
 		});
 		
+		//These are the listeners
 		
 		frameHermes.getContentPane().add(MapPanel);
 		frameHermes.setVisible(true);
@@ -114,7 +129,7 @@ public class HermesUI extends JPanel{
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawImage(map, 0, 0, null);
-		
+	//Allows us to paint the image within the JLabel	
 	}
 	
 }
