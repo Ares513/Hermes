2534af0857e22c0e81ad1c915cbe2eac47c0d229
Vaz <fccinelli@wpi.edu>
2015-11-15 15:30:53 -0500

    Added two events to MapManagement which bootstrapper needs to listen for. These events may need some revision; should ask evan.

---
+++ src/com/team1ofus/hermes/DirectionSet.java
@@ -0,0 +1,5 @@
+/* contains a set of unique points (in the form (Cell, (x, y))) which specify a complete path. The exact form this data will take has not yet been determined. 
+ */
+public class DirectionSet {
+	
+}
--- src/com/team1ofus/hermes/MapManagement.java
+++ src/com/team1ofus/hermes/MapManagement.java
@@ -17,6 +17,34 @@ PathUI
 
  */
 
-public class MapManagement{
+public class MapManagement {
+	Cell[] cells = new Class[4];
 	
+	/* This event lets bootstrapper know when A* finishes computing a path and would like to display it. 
+	 * 
+	 */
+	public class AStarPathCompleteEvent extends EventObject {
+		private DirectionSet[] path;
+		AStarPathCompleteEvent(source, id, aPath) {
+			super(source, id);
+			path = aPath;
+		}
+		public DirectionSet[] getDirectionSet() {
+			return path;
+		}
+	}
+	
+	/* This event lets bootstrapper know when mapManagement wants to load a new cell from disk. 
+	 * 
+	 */
+	public class RequesteCellEvent extends EventObject {
+		private String cellName;
+		RequesteCellEvent(source, id, aCellName) {
+			super(source, id);
+			cellName = aCellName;
+		}
+		public String getCellName() {
+			return cellName;
+		}
+	}
 }
