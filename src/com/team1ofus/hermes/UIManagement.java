61caac7682cb62d6c5a4a86ce47a5f7fcdf58288
Aaron Jaeger <Aaron Jaeger>
2015-11-16 22:06:49 -0500

    first commit, still figuring stuff out

--- src/com/team1ofus/hermes/UIManagement.java
+++ src/com/team1ofus/hermes/UIManagement.java
@@ -1,10 +1,42 @@
 package com.team1ofus.hermes;
 
+import javax.swing.*;
+
+
 public class UIManagement {
 	HermesUI window;
 	public UIManagement(){
 		initialize();
 	}
+	
+	public JFrame frame; 
+	/*
+	 * public render CellRender; 
+	 * public Search searchUI
+	 * 
+	 */
+	
+	/*
+	 *  public void add(int val1, int val2){
+     *      synchronized(this){
+     *         this.sum1 += val1;   
+     *       }
+     *      synchronized(this){
+     *         this.sum2 += val2;
+     *       }
+     *    }
+	 * 
+	 */
+	public void render(){ 
+		synchronized(this){ 
+		/*
+		 * Stuff goes here
+		 */
+		}
+	}
+	
+	
+	
 	
 	void initialize(){
 		window = new HermesUI();
