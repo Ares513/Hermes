b0e13f181a1216519a590485fd47f33f5548a8c0
wsbarnard <wbarnard751@gmail.com>
2015-11-18 07:23:38 -0500

    2 A* tests working, still need to actually move between two different points but at least it's working as intended so far.

--- src/com/team1ofus/hermes/AStarTest.java
+++ src/com/team1ofus/hermes/AStarTest.java
@@ -9,16 +9,28 @@ import org.junit.Test;
 
 public class AStarTest {
 
-	@Test
-	public void test() {
-		PathCell testCell = new PathCell("bag'o'dicks",5,5,16, TILE_TYPE.WALL);
+	@Test // should return the 1 cell.(start and end are the same)
+	public void test() { 
+		PathCell testCell = new PathCell("bag'o'bags",5,5,16, TILE_TYPE.WALL);
 		ArrayList<PathCell> testCellList = new ArrayList<PathCell>();
 		testCellList.add(testCell);
 		AStar test = new AStar(testCellList);
-		System.out.println(test.getPath(0, new Point(2,2), 0, new Point(2,2)));
-//		ArrayList<PathCell> expected = new ArrayList<PathCell>();
-//		expected.add(
-//		assertEquals(test.getPath(0, new Point(2,2), 0, new Point(2,2)), )
+		ArrayList<CellPoint> AStarOut = test.getPath(0, new Point(2,2), 0, new Point(2,2));
+		for(CellPoint each:AStarOut){
+			System.out.println(each.getPoint());
+		}
+		assertEquals(0,0);
+	}
+		
+	@Test
+	public void test2() { // should return path not found, all walls
+		PathCell testCell = new PathCell("bag'o'schwifty",5,5,16, TILE_TYPE.WALL);
+		ArrayList<PathCell> testCellList = new ArrayList<PathCell>();
+		testCellList.add(testCell);
+		AStar test = new AStar(testCellList);
+		ArrayList<CellPoint> AStarOut = test.getPath(0, new Point(2,2), 0, new Point(2,3));
+		assertEquals(AStarOut,null);
+		
 	}
 
 }
