ddb60bcc62dc358d1c0557677811693ebdfa6e0f
Nicolas Adami-Sampson <nicolas.adamisampson@gmail.com>
2015-11-18 00:36:14 -0500

    Updated Sprite Sheet, and reorderd TILTE_TYPE

--- src/com/team1ofus/hermes/TILE_TYPE.java
+++ src/com/team1ofus/hermes/TILE_TYPE.java
@@ -2,8 +2,8 @@ package com.team1ofus.hermes;
 
 public enum TILE_TYPE {
 	WALL, //cannot path through a wall
-	DOOR,    // Will indicate start and end locations for rooms
 	PEDESTRIAN_WALKWAY, //can path through pedestrian hallway from any direction to any direction
+	DOOR,    // Will indicate start and end locations for rooms
 	GRASS,   //can path through grass. Will have a higher weight then walkway(harder to traverse)
 	CONGESTED, //Indicates congested areas. Will have a higher weight then walkway
 	STAIRS,    //Can path through stairs. Will be the tile that switches between maps
