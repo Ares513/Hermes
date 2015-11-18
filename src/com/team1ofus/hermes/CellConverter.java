d652d195d6d5b322429ef75caf63790c73a7030c
Aaron Jaeger <Aaron Jaeger>
2015-11-18 05:06:40 -0500

    changes to CellConverte

--- src/com/team1ofus/hermes/CellConverter.java
+++ src/com/team1ofus/hermes/CellConverter.java
@@ -1,15 +1,31 @@
 package com.team1ofus.hermes;
-
+/*
+ * converts cells from Apollo to cells for A*
+ */
 public class CellConverter {
 	public CellConverter(){	
 	}
-	/*
+	
+//	private static byte tileType(DataTile ConvertTiletype){
+//		if(ConvertTiletype.getType() == TILE_TYPE.WALL){ // currently the only non-traversible. 
+//			return 1;
+//		}
+//		else {
+//			return 0; 
+//		}
+//	}
 	public static PathCell convertCell (Cell newCell){ 
 		int convertWidth = newCell.getWidth(); 
 		int convertHeight = newCell.getHeight(); 
 		double convertScaling = newCell.getScale(); 
-		String name = newCell.getID();
-		return(name,convertWidth, convertHeight, convertScaling, Tile defaultTile);
+		String name = newCell.getID();		
+//		byte convertTyleType; 
+//		for(int i=0; i<newCell.getWidth(); i++) {
+//			for(int j=0; j<newCell.getHeight(); j++) {
+//				convertTyleType = tileType(newCell.tiles[i][j]);
+//			}
+//		}
+		return new PathCell(name,convertWidth, convertHeight, convertScaling);
 	}
-	*/
+	
 }
