fec83d13ff167053cce9a0701767402e5be8fc13
Vaz <fccinelli@wpi.edu>
2015-11-17 20:17:23 -0500

    Once again adding some stuff I forgot to include in the last commit.

---
+++ src/com/team1ofus/hermes/IAStarRequestCellListener.java
@@ -0,0 +1,5 @@
+package com.team1ofus.hermes;
+
+public interface IAStarRequestCellListener {
+	void onAStarRequestCellEvent(String cellName);
+}
---
+++ src/com/team1ofus/hermes/ICellLoadedListener.java
@@ -0,0 +1,5 @@
+package com.team1ofus.hermes;
+
+public interface ICellLoadedListener {
+	void onCellLoaded(Cell cell);
+}
