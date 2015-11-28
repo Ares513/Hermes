d442fb96fb73d6a8e3e5d86de2e0253894041fee
Aaron Jaeger <Aaron Jaeger>
2015-11-28 17:55:40 -0500

    takes a* path, breaks each step into a direction. n,s,e,w,ne,nw,se,sw. Then takes each step and adds consecutive steps in the same direction together and calculates distance in that direction. A little buggy. trying to figure out isnt working.

---
+++ src/com/team1ofus/hermes/Directions.java
@@ -0,0 +1,19 @@
+package com.team1ofus.hermes;
+
+import java.lang.String;
+public class Directions { 
+	public String heading; 
+	public double distance; 
+	
+	public Directions(){ 
+		
+	}
+	public String getHeading(){ 
+		return this.heading; 
+	}
+	
+	public double getDistance(){ 
+		return this.distance; 
+	}
+	
+}
--- src/com/team1ofus/hermes/PrintDirections.java
+++ src/com/team1ofus/hermes/PrintDirections.java
@@ -2,48 +2,113 @@ package com.team1ofus.hermes;
 
 import java.awt.Point;
 import java.util.ArrayList;
-
-import javax.swing.*;
+import java.lang.String;
+//import javax.swing.*;
 
 public class PrintDirections { 
-	public PrintDirections() { 
-	}
+	//public enum Direction{FORWARD,BACKWARD,LEFT,RIGHT,F_LEFT,F_RIGHT,B_LEFT,B_RIGHT}
 	
+	public PrintDirections() { 
+	}	
+	 
 	
 	public void parseDirections(ArrayList<CellPoint> AStarDirections){
 		int numberOfSteps = AStarDirections.size(); 
-		for(int i = 0; i < numberOfSteps; i++){ 
+		ArrayList<String> stateList = new ArrayList<String>(); 
+		for(int i = 1; i < numberOfSteps; i++){ 
 			CellPoint iterationI = AStarDirections.get(i);
-			Point thisPoint = iterationI.getPoint();
-			double xCoord = thisPoint.getX();
-			double yCoord = thisPoint.getY();
+			CellPoint prevIteration = AStarDirections.get(i-1); 
+			double xDiff = getXDifference(iterationI,prevIteration);
+			double yDiff = getYDifference(iterationI,prevIteration); 
+			String state = getState(xDiff, yDiff); 
+			stateList.add(state); 
+		}
+		ArrayList<Directions> directionsList = route(stateList);
+		int dListSize = directionsList.size(); 
+		for(int i = 0; i < dListSize; i++){ 
+			Directions currentInstruction = directionsList.get(i); 
+			double currentDistance = currentInstruction.getDistance(); 
+			String currentState = currentInstruction.getHeading();
 			
-			System.out.print(xCoord);
+			System.out.print(currentState);
 			System.out.print(",");
-			System.out.println(yCoord);
+			System.out.println(currentDistance);
 		}
 	}
-	private double getDifference(CellPoint currentP, CellPoint previousP){ 
+	private double getXDifference(CellPoint currentP, CellPoint previousP){ 
 		Point currentPoint = currentP.getPoint();
 		double xC = currentPoint.getX();
-		double yC = currentPoint.getY();
 		Point previousPoint = previousP.getPoint();
 		double xP = previousPoint.getX(); 
-		double yP = previousPoint.getY(); 
-		
 		double diffX = xC - xP; 
+		return diffX; 
+	}
+	private double getYDifference(CellPoint currentP, CellPoint previousP){ 
+		Point currentPoint = currentP.getPoint();
+		double yC = currentPoint.getY();
+		Point previousPoint = previousP.getPoint();
+		double yP = previousPoint.getY(); 
 		double diffY = yC - yP; 
-	  
-		return 0; 
+		return diffY; 
 	}
 	
+	private String getState(double xChange, double yChange){ 
+		String direction = ""; 
+		if(yChange > 0 ){ 
+			direction+="S";
+		}
+		if(yChange < 0){ 
+			direction += "N";
+		}
+		if(xChange > 0 ){
+			direction+="E"; 
+		}
+		if(xChange < 0 ){
+			direction+="W"; 
+		}
+		return direction; 
+	}
+	
+	private double getDistance(String state){ 
+		if(state.length() == 1){ 
+		return 3; //1 tile = 3 feet
+		} 
+		else { 
+			return 4.24; //sqrt(3^2 + 3^2). distance of diagonal. 
+		}
+	}
+	
+	private Directions newDirection(String heading, double distance){ 
+		Directions newEntry = new Directions(); 
+		newEntry.heading = heading; 
+		newEntry.distance = distance; 
+		return newEntry; 
+	}
+	
+	private ArrayList<Directions> route(ArrayList<String> states){ 
+		int size = states.size();
+		ArrayList<Directions> dList = new ArrayList<Directions>();
+		double currentDistance =0; 
+		
+		for(int i = 0; i < size; i++){ 
+			String currentState = states.get(i); 
+		
+			if(i > 0){ 
+				String previousState = states.get(i-1); 
+				if(currentState.equals(previousState) && i!= (size-1)){ 
+					currentDistance += getDistance(currentState); 
+				}
+				else{ 
+					dList.add(newDirection(previousState, currentDistance)); 
+					currentDistance = 0; 
+				}
+			}
+			else{ 
+				currentDistance += getDistance(currentState); 
+			}
+		}
+		return dList; 
+	}
 }
 
-/*
- * (0,0)(1,0)(2,0)(3,0)(4,0) 
- * (0,1)(1,1)(2,1)(3,1)(4,1)
- * (0,2)(1,2)(2,2)(3,2)(4,2)
- * (0,3)(1,3)(2,3)(3,3)(4,3)
- * (0,4)(1,4)(2,4)(3,4)(4,4)
- * 
- */ 
+
