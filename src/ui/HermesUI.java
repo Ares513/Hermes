cc04d575096e09f1e931b8a8240725f9145d1545
Cuong Nguyen <cuongnguyen@NEWMACPRO.WIFI.WPI.EDU>
2015-12-13 20:53:13 -0500

    searching location can match full name with abbreviation

--- src/ui/HermesUI.java
+++ src/ui/HermesUI.java
@@ -729,12 +729,35 @@ public class HermesUI extends JPanel implements IHumanInteractionListener{
 		tabbedPane.getSelectedTabPane().getSelectedTabPane().humanInteractive.addListener(this, "HermesUI through addListenerToSelectedTab");
 	}
 	
+	String matchingLocation(String s){
+		HashMap<String, String> abb = new HashMap<String, String>();
+		abb.put("Campus Center", "CC");
+		abb.put("Atwater Kent Laboratories", "AK");
+		abb.put("Boynton Hall", "BH");
+		abb.put("Fuller Laboratories", "FL");
+		abb.put("Gordon Library", "GL");
+		abb.put("Higgin House", "HH");
+		abb.put("Project Center", "PC");
+		abb.put("Stratton Hall", "SH");
+		
+		for (String key:abb.keySet()){
+			if (s.contains(key)){
+				s = s.replace(key, abb.get(key));
+			}
+		}
+		System.out.println(s);
+		return s;
+	}
+	
 	class SearchListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String startLocation = (String) startPoint.getSelectedItem();
+			startLocation = matchingLocation(startLocation);
+
 			DebugManagement.writeNotificationToLog("start location is : " + startLocation);
 			String destLocation = (String) destination.getSelectedItem();
+			destLocation = matchingLocation(destLocation);
 			searchStartRecord = locationNameInfoRecords.get(0);
 			searchEndRecord = locationNameInfoRecords.get(0);
 		    for (Record r:locationNameInfoRecords){
