e71ea14590c93e739b83b17c3ac71c1f89473c5c
wsbarnard <wbarnard751@gmail.com>
2015-12-14 22:31:25 -0500

    Got rid of intermediate floors in paths. Also put a few updated maps in

--- src/pathing/AStar.java
+++ src/pathing/AStar.java
@@ -360,30 +360,38 @@ public class AStar {
 	}
 	
 	private ArrayList<CellPoint> cleanPath(ArrayList<CellPoint> roughPath) {
-		ArrayList<CellPoint> cleanPath = roughPath;
-		ArrayList<CellPoint> badTiles = new ArrayList<CellPoint>();
+		ArrayList<CellPoint> cleanPath = new ArrayList<CellPoint>();
 		String currentCellName = roughPath.get(0).getCellName();
 		String aCellName = null;
 		ArrayList<CellPoint> tilesInCurCell = new ArrayList<CellPoint>();
 		for(CellPoint each: roughPath){
 			aCellName = each.getCellName(); 
-			if(aCellName.equals(currentCellName)){
-				tilesInCurCell.add(each);
-			}
-			else{
+			if(!(aCellName.equals(currentCellName))){
 				currentCellName = aCellName;
-				if(tilesInCurCell.size() <= 2){
-//					cleanPath.removeAll(tilesInCurCell);
-					badTiles.addAll(tilesInCurCell);
+				if(tilesInCurCell.size() > 2){
+					cleanPath.addAll(tilesInCurCell);
 				}
 				tilesInCurCell = new ArrayList<CellPoint>();
-			}
+			} 
+			tilesInCurCell.add(each);
 		}
-		DebugManagement.writeNotificationToLog(String.valueOf(cleanPath.size()));
-		DebugManagement.writeNotificationToLog(String.valueOf(badTiles.size()));
-		cleanPath.removeAll(badTiles);
-		DebugManagement.writeNotificationToLog(String.valueOf(cleanPath.size()));
+		cleanPath.addAll(tilesInCurCell);
+//		DebugManagement.writeNotificationToLog(String.valueOf(cleanPath.size()));
 		return cleanPath;
+		
+		/*
+		aCellName = each.getCellName(); 
+		if(aCellName.equals(currentCellName)){
+			tilesInCurCell.add(each);
+		}
+		else{
+			currentCellName = aCellName;
+			if(tilesInCurCell.size() > 2){
+				cleanPath.addAll(tilesInCurCell);
+			}
+			tilesInCurCell = new ArrayList<CellPoint>();
+			tilesInCurCell.add(each);
+		} */
 	}
 	
 	private TileInfo buildTileInfo(CellPoint currentPoint) {
--- src/pathing/AStarTest.java
+++ src/pathing/AStarTest.java
@@ -163,4 +163,74 @@ public class AStarTest {
 			fail("A* returned null when it should not have");
 		}
 	}
+	
+	@Test
+	public void test6() {
+		System.out.println("test6: testing eliminating intermediate elevator/stair cells");
+		HashMap<Point, TILE_TYPE> hm1 = new HashMap<Point, TILE_TYPE>();
+		for(int i =0; i<100;i++){
+			for(int j = 0; j < 100; j++){
+				hm1.put(new Point(i,j), TILE_TYPE.ELEVATOR);
+			}
+		}
+		EntryPoint ep1 = new EntryPoint("A", new Point(45,45));
+		EntryPoint ep11 = new EntryPoint("B", new Point(45,45));
+		ArrayList<EntryPoint> eps1 = new ArrayList<EntryPoint>();
+		eps1.add(ep1);
+		eps1.add(ep11);
+		
+		EntryPointReference epr1 = new EntryPointReference("C","pc1", new Point(45,45));
+		EntryPointReference epr11 = new EntryPointReference("D","pc1", new Point(45,45));
+		ArrayList<EntryPointReference> eprs1 = new ArrayList<EntryPointReference>();
+		eprs1.add(epr1);
+		eprs1.add(epr11);
+		
+		PathCell pc1 = new PathCell("pc1", "pc1", 100, 100, hm1, eps1, new ArrayList<LocationNameInfo>(), eprs1);
+		
+		EntryPoint ep2 = new EntryPoint("C", new Point(45,45));
+		EntryPoint ep22 = new EntryPoint("D", new Point(45,45));
+		EntryPoint ep222 = new EntryPoint("J", new Point(45,45));
+		ArrayList<EntryPoint> eps2 = new ArrayList<EntryPoint>();
+		eps2.add(ep2);
+		eps2.add(ep22);
+		eps2.add(ep222);
+		
+		EntryPointReference epr2 = new EntryPointReference("A","pc1", new Point(45,45));
+		EntryPointReference epr22 = new EntryPointReference("B","pc1", new Point(45,45));
+		EntryPointReference epr222 = new EntryPointReference("I","pc3", new Point(45,45));
+		ArrayList<EntryPointReference> eprs2 = new ArrayList<EntryPointReference>();
+		eprs2.add(epr2);
+		eprs2.add(epr22);
+		eprs2.add(epr222);
+		
+		PathCell pc2 = new PathCell("pc2", "pc2", 100, 100, hm1, eps2, new ArrayList<LocationNameInfo>(), eprs2);
+		
+		EntryPoint ep3 = new EntryPoint("I", new Point(45,45));
+		ArrayList<EntryPoint> eps3 = new ArrayList<EntryPoint>();
+		eps3.add(ep3);
+		
+		EntryPointReference epr3 = new EntryPointReference("J","pc2", new Point(45,45));
+		ArrayList<EntryPointReference> eprs3 = new ArrayList<EntryPointReference>();
+		eprs3.add(epr3);
+		
+		PathCell pc3 = new PathCell("pc3", "pc3", 100, 100, hm1, eps3, new ArrayList<LocationNameInfo>(), eprs3);
+		
+		
+		ArrayList<PathCell> pcs = new ArrayList<PathCell>();
+		pcs.add(pc1);
+		pcs.add(pc2);
+		pcs.add(pc3);
+		
+		AStar test = new AStar(pcs, "pc2", new AStarConfigOptions());
+		ArrayList<CellPoint> AStarOut = test.getPath(new CellPoint("pc3", new Point(50, 50)),
+				new CellPoint("pc1", new Point(55, 40)), false);
+		if (AStarOut.size() >= 5) {
+			System.out.println("getPath didnt break");
+			for (CellPoint each : AStarOut) {
+				System.out.println(each.getCellName() + each.getPoint());
+			}
+		} else {
+			fail("A* returned null when it should not have");
+		}
+	}
 }
